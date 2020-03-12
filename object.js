c8b879459dc44db47ca5a9d7e6bd3cbea4a44cc1
David Lovemore <davidlovemore@gmail.com>
2020-03-12 17:19:36 +0000

    defining functions and setting variables...

--- object.js
+++ object.js
@@ -1,4 +1,5 @@
-var p=console.log
+var p=print=console.log
+
 function thing(p) {
   for(var a in p) {
     this[a]=p[a]
@@ -17,10 +18,9 @@ function mapd(f,o) { return map(f,o,{}); }
 
 function copyd(o) { return map(I,o); }
 
-
 function fun(o) {
-  var f=new thing[o]
-  f.f=eval('function'+o.args+'{'+o.code+'}')
+  var f=new thing(o)
+  f.f=eval('(function'+o.args+'{ var v={};'+o.code+';})')
   return f
 }
 
@@ -29,12 +29,24 @@ function I(x) { return x; }
 function rebuild(o) {
   if(typeof o!='object') return o
   if ('!' in o) {
-    return eval(o[!])(o)
+    return eval(o['!'])(o)
   } else return o
 }
 
 function def(d) {
-  return new thing(mapd(rebuild,o))
+  return new thing(mapd(rebuild,d))
 }
 
+function d(s,v) {
+  x={v:v}
+  eval(s+'=x.v')
+}
 
+p(function(x) {  return x;})
+p(eval('(function(x) { return x;})'))
+p(def({'a':1, b:2}))
+p(def({'a':1, b:2, f: {'!':'fun', 'args': '(x)', 'code': 'return x+1'}}))
+p(eval('x=3'))
+p(eval('x'))
+p(d('zz',12))
+p(zz)
